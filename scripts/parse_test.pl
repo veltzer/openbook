78a50a3ca44b427bd1f6cbe0044a40d1637992dc
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-18 01:42:59 +0300

    better parsing of lilypond headers

---
+++ scripts/parse_test.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+
+# This is a script to test the parsing of lilypond
+
+use strict;
+use diagnostics;
+use Perl6::Slurp qw();
+use Parse::RecDescent qw();
+
+my($debug)=1;
+
+if(@ARGV!=1) {
+	die("usage: infile");
+}
+my($infile)=$ARGV[0];
+
+if($debug) {
+	print "infile is [$infile]\n";
+}
+
+my($grammer);
+my($grammer_file)="data/lilypond.grammer";
+$grammer=Perl6::Slurp::slurp($grammer_file);
+#print "grammer is $grammer";
+$::RD_HINT=1;
+$::RD_WARN=1;
+#$::RD_TRACE=1;
+#$Parse::RecDescent::skip='[ \v\t\n]*';
+my($data);
+$data=Perl6::Slurp::slurp($infile);
+my($parser)=Parse::RecDescent->new($grammer);;
+my($ret)=$parser->lilyfile(\$data);
+if(!$ret) {
+	die("ERROR!");
+}
+if($debug) {
+	print("printing returned hash...\n");
+	while(my($key,$val)=each(%$ret)) {
+		print("key is $key, val is $val\n");
+	}
+}
+# check that there is no more data which is not parsed...
+if($data!~/^\s*/) {
+	die("rest of data is [$data]");
+}
+if($debug) {
+	print "done...\n";
+}
