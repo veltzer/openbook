97565387ac15254f2a651945b55ca4daf6fea39b
Mark Veltzer <mark.veltzer@gmail.com>
2011-09-18 11:52:27 +0300

    added creation of book

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,4 +3,10 @@
 # this scrip will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-sudo apt-get install lilypond lilypond-doc timidity lame gpp m4 cpp
+# packages and their reasons:
+# lilypond: lilypond
+# pdfjam: pdfjoin
+
+# optional:
+# lilypond-doc: for documentation
+sudo apt-get install lilypond timidity lame gpp m4 cpp pdfjam
