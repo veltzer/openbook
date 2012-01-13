63b4bcbc6798825947f5f7e20ffc272c3aa37985
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-14 01:44:21 +0200

    better building

--- scripts/ubuntu_install.py
+++ scripts/ubuntu_install.py
@@ -21,17 +21,19 @@ packs=[
 	'perl', # for the perl scripts
 	'python', # for the python scripts
 	'python-mako', # this is out template processor
-#	'python3-mako',
-#	'python-mako-doc',
+	'python3-mako', # mako for python 3
+	'python-mako-doc', # documentation for the template preprocessor
 	'lilypond', # this is the main tool we use
 	'python-tweepy', # this is for tweeting new releases
 	'python-tweepy-doc', # this is for tweeting new releases
 	'python-facebook', # this is for facebooking releases
 	'python-simplejson', # this make facebook work better
-#	'lilypond-doc',
-#	'timidity',
-#	'lame',
-#	'pdfjam',
+	'texi2html', # needed to install lilypond from source
+	'guile-1.8-dev', # needed to install lilypond from source
+	'lilypond-doc', # documentation for lilypond
+	'timidity', # play output midi
+	'lame', # convert audio
+	'pdfjam', # join pdfs
 ]
 
 args=['sudo','apt-get','install']
