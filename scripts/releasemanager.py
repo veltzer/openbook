b457bdb26b4c207e4986873bf099a390804ef2f1
Mark Veltzer <mark@veltzer.net>
2012-04-12 06:24:06 +0300

    more code

--- scripts/releasemanager.py
+++ scripts/releasemanager.py
@@ -18,6 +18,7 @@ class ReleaseManager:
 		self.p_from=self.config.get(self.section,'from')
 		self.p_to=self.config.get(self.section,'to').split(',')
 		self.p_smtp_host=self.config.get(self.section,'smtp_host')
+		self.p_smtp_port=self.config.get(self.section,'smtp_port')
 		self.p_content=self.config.get(self.section,'content')
 		self.p_mail_user=self.config.get(self.section,'mail_user')
 		self.p_mail_password=self.config.get(self.section,'mail_password')
@@ -40,30 +41,39 @@ class ReleaseManager:
 			#print the entire config
 			self.config.write(sys.stdout)
 			self.p_debug=True
-	def send_email(self):
+	def doEmail(self):
 		# build the message...
 		msg=email.mime.text.MIMEText(self.p_content)
 		msg['Subject']=self.p_subject
 		msg['From']=self.p_from
 		# Send the message via our own SMTP server, but don't include the # envelope header.
-		server=smtplib.SMTP(self.p_smtp_host)
+		server=smtplib.SMTP(self.p_smtp_host,self.p_smtp_port)
+		server.ehlo()
 		if(self.p_debug):
 			server.set_debuglevel(1)
 		if(self.p_usetls):
 			server.starttls()
+		server.ehlo()
+		if(self.p_debug):
+			print('p_mail_user is ',self.p_mail_user)
+			print('p_mail_password is ',self.p_mail_password)
 		server.login(self.p_mail_user,self.p_mail_password)
 		server.sendmail(self.p_from,self.p_to,msg.as_string())
 		server.quit()
-	def send_tweet(self):
+	def doTweet(self):
 		auth=tweepy.OAuthHandler(self.p_twitter_consumer_key,self.p_twitter_consumer_secret)
 		auth.set_access_token(self.p_twitter_access_token,self.p_twitter_access_token_secret)
 		api=tweepy.API(auth)
 		api.update_status(self.p_content)
+	def doFace(self):
+		pass
 	def release(self):
 		if(self.p_email):
-			self.send_email()
+			self.doEmail()
 		if(self.p_tweet):
-			self.send_tweet()
+			self.doTweet()
+		if(self.p_face):
+			self.doFace()
 
 if __name__=='__main__':
 	rm=ReleaseManager()
