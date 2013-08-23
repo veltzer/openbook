a98108ca161ecda5e051b4a67af2b32258d00e90
Mark Veltzer <mark@veltzer.net>
2013-08-23 23:21:22 +0300

    credit Jordan

--- scripts/versioncheck.py
+++ scripts/versioncheck.py
@@ -4,7 +4,7 @@ import sys # for version_info
 
 def checkversion():
 	if sys.version_info[0:2] != (2,7):
-		raise Exception('you must use python 2.7 (ubuntu 11 or 12 carry it)')
+		raise Exception('you must use python 2.7 (ubuntu 11, 12, 13 carry it)')
 
 #if __name__=='__main__':
 #	checkversion()
