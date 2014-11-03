3757d46706f1a3b64624bb27036120b483e33f9b
Mark Veltzer <mark@veltzer.net>
2014-11-03 20:37:57 +0200

    scripts change

--- scripts/wrapper_ok.py
+++ scripts/wrapper_ok.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 
-# run any command line and do not emit it's standard error or output unless there is an error
+'''
+run any command line and do not emit it's standard error or output unless there is an error
+'''
 
 import sys # for argv
-import subprocess # for call
+import subprocess # for check_output
 
-subprocess.call(sys.argv[1:])
+if len(sys.argv)<2:
+	raise ValueError('command line issue')
+
+# run the command
+subprocess.check_output(sys.argv[1:])
