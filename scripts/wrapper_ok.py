84d699ebe2377f07579f30e29e26f67b73110cfc
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-03 20:41:54 +0200

    fixed this wrapper

--- scripts/wrapper_ok.py
+++ scripts/wrapper_ok.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 '''
-run any command line and do not emit it's standard error or output unless there is an error
+run any command line, emit its output and error but always return an OK (0) return code
 '''
 
 import sys # for argv
-import subprocess # for check_output
+import subprocess # for call
 
 if len(sys.argv)<2:
 	raise ValueError('command line issue')
 
 # run the command
-subprocess.check_output(sys.argv[1:])
+subprocess.call(sys.argv[1:])
