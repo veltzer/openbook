019db3da5f1c98e9aecb08993de83623176bef3d
Mark Veltzer <mark@veltzer.net>
2014-11-06 16:46:27 +0200

    updated the release script

--- scripts/release.py
+++ scripts/release.py
@@ -1,56 +1,71 @@
 #!/usr/bin/python3
 
-# this is a release script.
-# it runs git status -s in order to see that everything is commited.
-# it then tags the current tree with one + the old tag.
-# it then cleans and then rebuilds everything and puts the results in the output.
-# we use -a so that the tags will be annotated and therefor seen in all branches
-# (say gh-pages branch)
+'''
+this is a release script.
+it runs git status -s in order to see that everything is commited.
+it then tags the current tree with one + the old tag.
+it then cleans and then rebuilds everything and puts the results in the output.
+we use -a so that the tags will be annotated and therefor seen in all branches
+(say gh-pages branch)
 
-# TODO:
-# - add integration with twitter and facebook to announce new versions.
-# - try to use a better git interface (there are native python git interfaces).
+TODO:
+- add integration with twitter and facebook to announce new versions.
+- try to use a better git interface (there are native python git interfaces).
+'''
 
-# this is for running the various commands that we need
+###########
+# imports #
+###########
 import subprocess # for check_output, check_call
 import os # for getcwd
 import check_version # for check_version
+import sys # for stderr, exit
 
 ##############
 # parameters #
 ##############
 # do you want debug info printed?
-debug=True
+opt_debug=True
 # do you want to check if everything is commited ? Answer True to this
 # unless you are doing development on this script...
-check=True
+opt_check=True
 # what is the name of the project?
-project=os.getcwd().split('/')[-1]
+opt_project=os.getcwd().split('/')[-1]
 # do we want to use the release manager?
-doRelease=False
+opt_release=False
 
 ########
 # code #
 ########
+
 # first check that we are using the correct version of python
 check_version.check_version()
 
-if check:
-	out=subprocess.check_output(['git','status','-s']).decode()
+# check that everything is committed
+if opt_check:
+	out=subprocess.check_output(['git','status','-s']).decode().rstrip()
 	if out!='':
-		raise ValueError('first commit everything, then call me...')
-tag=int(subprocess.check_output(['git','describe','--abbrev=0']).decode().strip())
-if debug:
-	print('old tag is '+str(tag))
+		print('first commit everything, then call me...', file=sys.stderr)
+		sys.exit(1)
+
+# tag the new version
+tag=int(subprocess.check_output(['git','describe','--abbrev=0']).decode().rstrip())
+if opt_debug:
+	print('old tag is [{0}]'.format(tag))
 tag+=1
-if debug:
-	print('new tag is '+str(tag))
+if opt_debug:
+	print('new tag is [{0}]'.format(tag))
 tag=str(tag)
-# tag the new tag
-subprocess.check_output(['git','tag','-a','-s','-m',project+' version '+tag,tag])
+subprocess.check_output(['git','tag','-a','-s','-m',opt_project+' version '+tag,tag])
+
+# clean
 subprocess.check_call(['make','clean'])
+
+# build and install
 subprocess.check_call(['make','install'])
-if doRelease:
+
+# call the release manager
+if opt_release:
 	import releasemanager # for ReleaseManager
 	rm=releasemanager.ReleaseManager()
 	rm.release()
