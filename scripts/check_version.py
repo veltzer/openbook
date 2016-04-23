de56c54aee7e0bd6cc0dd78c6ca9823d3508e0bd
Mark Veltzer <mark@veltzer.net>
2016-04-23 18:24:01 +0300

    updated python version to 3.5

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -3,7 +3,7 @@
 import sys # for version_info
 
 def check_version():
-	version_needed=(3,4)
+	version_needed=(3,5)
 	if sys.version_info[0:2] != version_needed:
 		raise Exception('you must use python {ver}'.format(
 			ver=version_needed,
