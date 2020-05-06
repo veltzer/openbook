7a24860674f95b6a732f8488ace06e8015d175d5
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-07 00:57:40 +0300

    fixed typo

--- scripts/update_pdf_viewer.py
+++ scripts/update_pdf_viewer.py
@@ -8,10 +8,13 @@ import config.openbook
 temp_file="/tmp/viewer.zip"
 if os.path.exists(temp_file):
     os.unlink(temp_file)
+url=(
+    "https://github.com/mozilla/pdf.js/releases/download/"
+    "v{0}/pdfjs-{0}-dist.zip".format(config.openbook.pdfjs_version)
+)
 subprocess.check_call([
     "wget",
-    """https://github.com/mozilla/pdf.js/releases/download/
-    v{}/pdfjs-${}-dist.zip""".format(config.openbook.pdfjs_version),
+    url,
     "-O",
     temp_file,
 ])
