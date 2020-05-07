929efc0ea5b29c3107f8f9a35ced66744b87b840
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-07 08:59:27 +0300

    16.04 does not have good enough python

--- config/python.py
+++ config/python.py
@@ -26,7 +26,7 @@ dev_requires = [
 # test_os="[ubuntu-16.04, ubuntu-18.04, macos-10.15]"
 test_os="[ubuntu-16.04, ubuntu-18.04]"
 test_python="[3.6, 3.7, 3.8]"
-test_container="[ 'ubuntu:16.04', 'ubuntu:18.04', 'ubuntu:20.04' ]"
+test_container="[ 'ubuntu:18.04', 'ubuntu:20.04' ]"
 
 python_requires = ">=3"
 
