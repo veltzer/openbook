43fa9aed0728f13cf3e1e0ec1fcc4e18320f78b0
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-07 08:29:05 +0300

    better script

--- scripts/build_on_docker.sh
+++ scripts/build_on_docker.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 # update must be done first or it doesn't know anything about any package
 apt-get update
 # because tzdata is interactive
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install tzdata
 # this is the real installation
-apt-get install -y lilypond qpdf python3 python3-pip python3-virtualenv git
+apt-get install -y lilypond qpdf python3 python3-pip virtualenv git
 # create the virtual env
-python3 -m virtualenv .venv
+virtualenv -p /usr/bin/python3 .venv
 # enter the virtual env
 source .venv/bin/activate
 # upgrate pip
