2d1b397a02a184be4546bcfe98741584cfbc858d
Mark Veltzer <mark.veltzer@gmail.com>
2020-08-07 14:18:23 +0300

    bug in config/git.py

--- config/git.py
+++ config/git.py
@@ -10,7 +10,7 @@ import subprocess
 
 git_last_tag = subprocess.check_output(
     ["git", "describe", "--abbrev=0", "--tags"], stderr=subprocess.DEVNULL
-).rstrip()
+).decode().rstrip()
 git_describe = subprocess.check_output(
     ["git", "describe"], stderr=subprocess.DEVNULL
 ).decode().rstrip()
