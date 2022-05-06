3e774de18fb7a8eba95b97b23fea8d1aa8b7a301
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:06 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,23 +2,18 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-run_requires = []
-setup_requires = []
-install_requires = []
-test_requires = []
-
 dev_requires = [
-    'pydmt',
-    'pytconf',
-    'python-dateutil',
-    'tqdm',
-    'PyMySQL',
-    'Mako',
-    'pymakehelper',
-    'requests',
-    'gitpython',
+    "pydmt",
+    "pytconf",
+    "python-dateutil",
+    "tqdm",
+    "PyMySQL",
+    "Mako",
+    "pymakehelper",
+    "requests",
+    "gitpython",
 ]
+
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
 test_python = ["3.9"]
