cf8557a191ddadd378b5105a517cd207009cc84a
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-14 19:01:15 +0300

    deps change

--- config/deps.py
+++ config/deps.py
@@ -1,28 +1,28 @@
 packages = {
     # must have (critical)
-    'python3': 'for the python scripts',
-    'virtualenv': 'for using virtualenv for python',
-    'lilypond': 'this is the main tool we use',
-    'qpdf': 'for qpdf(1) (used in pdf size reduction)',
+    "python3": "for the python scripts",
+    "virtualenv": "for using virtualenv for python",
+    "lilypond": "this is the main tool we use",
+    "qpdf": "for qpdf(1) (used in pdf size reduction)",
 
     # for producing extra stuff (not critical)
-    'ghostscript': 'for ps2pdf(1), pdf2ps(1), gv (used in pdf size reduction and cutting pdfs)',
-    'timidity': 'play output midi and convert midi to wav',
-    'lame': 'convert audio (encode ogg)',
+    "ghostscript": "for ps2pdf(1), pdf2ps(1), gv (used in pdf size reduction and cutting pdfs)",
+    "timidity": "play output midi and convert midi to wav",
+    "lame": "convert audio (encode ogg)",
 
     # for web pages creation (not critical)
-    'tidy': 'for tidy(1) (used in checking HTML)',
+    "tidy": "for tidy(1) (used in checking HTML)",
 
     # for development (optional, only for develpers)
-    'okular': 'kde pdf reader',
-    'python-mako-doc': 'documentation for the template preprocessor',
-    'texi2html': 'needed to install lilypond from source',
-    # 'guile-2.0-dev': 'needed to install lilypond from source',
-    'guile-2.2-dev': 'needed to install lilypond from source',
-    'lilypond-doc': 'documentation for lilypond',
-    'lilypond-doc-html': 'documentation for lilypond',
-    'lilypond-doc-pdf': 'documentation for lilypond',
+    "okular": "kde pdf reader",
+    "python-mako-doc": "documentation for the template preprocessor",
+    "texi2html": "needed to install lilypond from source",
+    # "guile-2.0-dev": "needed to install lilypond from source",
+    "guile-2.2-dev": "needed to install lilypond from source",
+    "lilypond-doc": "documentation for lilypond",
+    "lilypond-doc-html": "documentation for lilypond",
+    "lilypond-doc-pdf": "documentation for lilypond",
 
     # npm is used for htmlhint
-    'npm': 'for htmlhint',
+    "npm": "for htmlhint",
 }
