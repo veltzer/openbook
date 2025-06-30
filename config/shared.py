72ac2f8dce258258f1d8874ff210569f8aa64ae5
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 12:56:12 +0300

    fixed up deps

--- config/shared.py
+++ config/shared.py
@@ -3,15 +3,31 @@
 PCONFIG: list[str] = [
     "pyclassifiers",
 ]
+# build for python module
 PBUILD: list[str] = [
     "hatch",
     "pydmt",
     "pymakehelper",
+    "pycmdtools",
 ]
+# build for non python module
+BUILD: list[str] = [
+    "pydmt",
+    "pymakehelper",
+    "pycmdtools",
+]
+# testing for python modules
 PTEST: list[str] = [
     "pylint",
     "pytest",
     "mypy",
     "ruff",
     # "pytest-cov",
+]
+# testing for non python modules
+TEST: list[str] = [
+    "pylint",
+    "pytest",
+    "mypy",
+    "ruff",
 ]
