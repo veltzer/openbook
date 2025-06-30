dba7e9ce846a98b36494b3b7d1f686902a538fb0
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 11:06:37 +0300

    sync

---
+++ config/shared.py
@@ -0,0 +1,17 @@
+""" shared stuff """
+
+PCONFIG: list[str] = [
+    "pyclassifiers",
+]
+PBUILD: list[str] = [
+    "hatch",
+    "pydmt",
+    "pymakehelper",
+]
+PTEST: list[str] = [
+    "pylint",
+    "pytest",
+    "mypy",
+    "ruff",
+    # "pytest-cov",
+]
