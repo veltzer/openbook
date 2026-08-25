9b48b62da7c5af22a32523820af77c2141b6404a
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-25 14:01:41 +0300

    converge config/personal.lua on the shared version
    The identity here is the same in every repo, so the file should be too.
    This copy was a trimmed variant missing DOMAIN, ORIGIN, WWW_HOSTNAME,
    WWW_DOMAINNAME, JSDOC_AUTHOR and the explanatory comments. Now guarded
    by the config-personal-lua rule in rsmultigit check-same.

--- config/personal.lua
+++ config/personal.lua
@@ -1,12 +1,28 @@
+-- Personal definitions
 FIRSTNAME = "Mark"
 SURNAME = "Veltzer"
 FULLNAME = FIRSTNAME .. " " .. SURNAME
 EMAIL = "mark.veltzer@gmail.com"
+DOMAIN = "veltzer.name"
 SLUG = "veltzer"
 
 GITHUB_USERNAME = "veltzer"
 LAUNCHPAD_USERNAME = "mark-veltzer"
 
+-- this is the key used for signing.
+-- it really should not have a passphrase since that will means that I have
+-- to manually sign everything.
+-- it must match the email_key up.
 KEY = "73C128F9"
 EMAIL_KEY = "mark.veltzer@gmail.com"
+-- the person who is the owner of the key and must match *exactly* what you get
+-- from "gpg --list-keys".
 KEY_SIGNER = FULLNAME .. " (signkey, never expires) <" .. EMAIL_KEY .. ">"
+
+ORIGIN = FULLNAME .. " <" .. EMAIL .. ">"
+-- hostname as seen from the outside world
+WWW_HOSTNAME = DOMAIN
+-- name as seen from the outside world
+WWW_DOMAINNAME = DOMAIN
+-- jsdoc style author tag
+JSDOC_AUTHOR = EMAIL .. " (" .. FULLNAME .. ")"
